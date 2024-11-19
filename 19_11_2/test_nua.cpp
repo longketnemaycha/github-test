d1298fc67947985f3ea3fdcff349bd9bb59a3fce
longketnemaycha <long.tran@rtrobotics.me>
2024-11-19 15:16:51 +0700

    test á

---
+++ 19_11_2/test_nua.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int so1, so2, tong;
+    cout <<"nhap 2 so ";
+    cin >> so1 >> so2;
+    tong = so1 + so2;
+
+    //in
+    cout << so1 << "+" << so2 << "=" << tong;
+
+
+    return 0;
+
+}
